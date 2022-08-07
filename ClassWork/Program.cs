1e1818c324f3ded9a40db5d64fb61de4d91d8126
Tatiana <ttt-ttt@bk.ru>
2022-08-07 17:46:59 +0300

    added HomeWork

--- ClassWork/Program.cs
+++ ClassWork/Program.cs
@@ -1,4 +1,31 @@
-﻿
+﻿void FillArray(int[,] array)
+{
+    Random rand = new Random();
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = rand.Next(0, 5);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
 
 void Task63()
 {
@@ -48,22 +75,77 @@ void Task67()
 {
     // Напишите программу, которая будет принимать на вход число и возвращать
     // сумму его цифр.
-    int number = 10002;
+    int number = 113001;
     int sum = 0;
     Console.Write($"Сумма цифр числа {number} равна ");
     Recursion(number);
     
     void Recursion(int num)
     {
-        if (num % 10 == 0) return;
-        int count = 0;
-        count = count + num % 10;
-        sum = sum + count;
+        if (num == 0) return;
+        sum = sum + num % 10;
         num = num / 10;
         Recursion(num);
     }
     Console.WriteLine(sum + ".");
 }
+
+void Task69()
+{
+    // Напишите программу, которая на вход принимает два числа A и B, и возводит 
+    // A в целую степень B с помощью рекурсии.
+    Console.WriteLine("Введите целое число - основание степени.");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите целое число - показатель степени.");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    Recursion(num1, num2);
+    void Recursion(int a, int b, int p = 1)
+    {
+        if (b == 0) 
+        {
+            Console.WriteLine($"Результат возведения числа {num1} в степень {num2} равен {p}.");
+            return;
+        }
+        p = p * a;
+        b--;
+        Recursion(a, b, p);
+    }  
+}
+
+void Task61()
+{
+    // Задайте две матрицы. Напишите программу, которая будет находить
+    // произведение двух матриц.
+    
+    Random rand = new Random();
+    int rowsFirst = rand.Next(3, 5);
+    int columnsFirst = rand.Next(3, 5);
+    int rowsSecond = columnsFirst;
+    int columnsSecond = rand.Next(3, 5);
+
+    int[,] arrayFirst = new int[rowsFirst, columnsFirst];
+    int[,] arraySecond = new int[rowsSecond, columnsSecond];
+    int[,] result = new int[rowsFirst,columnsSecond];
+
+    FillArray(arrayFirst);
+    FillArray(arraySecond);
+    PrintArray(arrayFirst);
+    PrintArray(arraySecond);
+
+    for (int i = 0; i < rowsFirst; i++)
+    {
+        for (int j = 0; j < columnsFirst; j++)
+        {
+            for (int k = 0; k < rowsSecond; k++)
+            {
+                result[i, j] += arrayFirst[i, k] * arraySecond[k, j];
+            }
+        }
+    }
+    PrintArray(result);
+}
 //Task63();
 //Task65();
-Task67();
+//Task67();
+//Task69();
+Task61();
---
+++ HomeWork/Program.cs
@@ -0,0 +1,70 @@
+﻿void Task64()
+{
+    // Задайте значения M и N. Напишите программу, которая выведет 
+    // все натуральные числа в промежутке от M до N, кратные трём.
+    int num1 = 1;
+    int num2 = 30;
+    Recursion(num1, num2);
+    Console.WriteLine();
+
+    void Recursion(int m, int n)
+    {
+        if (m > n) return;
+        if (m % 3 == 0)
+        {
+            Console.Write(m + " ");
+        }
+        m++;
+        Recursion(m, n);
+    }
+}
+
+void Task66()
+{
+    // Задайте значения M и N. Напишите программу, которая найдёт 
+    // сумму натуральных элементов в промежутке от M до N.
+    int num1 = 1;
+    int num2 = 10;
+    Recursion(num1, num2);
+
+    void Recursion(int m, int n, int sum = 0)
+    {
+        if (m > n)
+        {
+            Console.WriteLine($"Сумма натуральных чисел в промежутке от {num1} до {num2} равна {sum}.");
+            return;
+        }
+        sum = sum + m;
+        m++;
+        Recursion(m, n, sum);
+    }
+}
+
+void Task68()
+{
+    // Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+    // Даны два неотрицательных числа m и n.
+    int m = 3;
+    int n = 2;
+    Console.WriteLine($"Результат функции Аккермана А({m},{n}) равен {Ackerman(m, n)}."); 
+
+    int Ackerman(int m, int n)
+    { 
+    if (m == 0) 
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0) 
+    {
+        return Ackerman(m - 1, 1);
+    }
+    else 
+    {
+        return Ackerman(m-1, Ackerman(m, n-1)); 
+    }
+    }
+}
+
+Task64();
+Task66();
+Task68();
